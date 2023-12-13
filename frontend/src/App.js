e9a80b3650774101a1c9cbc209dc5e15d7accbc6
Hitesh764 <hiteshsahu5221@gmail.com>
2023-12-13 22:53:44 +0530

    Fourth Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './components/Recruitor/Auth/Login';
-import Candidatelogin from './components/Candidate/Auth/Candidatelogin'
+import Candidatelogin from './components/Candidate/Auth/Candidatelogin';
+import Nav from './components/Navbar/Nav';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav />
+
         <Routes>
           <Route path='/' element={<Login/>}/>
           <Route path='/can' element={<Candidatelogin/>}/>
